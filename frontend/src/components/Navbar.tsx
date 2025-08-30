83be8a1abfbdddff6c7ff28020e9929fe4f4e0bf
krilliams-521 <25971011+krilliams-521@users.noreply.github.com>
2025-08-29 22:54:50 -0500

    switching a tags to router links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <header className="bg-white shadow sticky top-0 z-10">
       <div className="max-w-5xl mx-auto px-4 py-4 flex justify-between items-center">
         <h1 className="text-xl font-bold text-gray-800">RecipeBase</h1>
         <nav className="space-x-4 text-sm text-gray-600">
-          <a href="#" className="hover:text-gray-800">
+          <Link to="/" className="hover:text-gray-800">
             Home
-          </a>
-          <a href="#" className="hover:text-gray-800">
+          </Link>
+          <Link to="#" className="hover:text-gray-800">
             Planner
-          </a>
-          <a href="#" className="hover:text-gray-800">
+          </Link>
+          <Link to="#" className="hover:text-gray-800">
             Login
-          </a>
+          </Link>
         </nav>
       </div>
     </header>
